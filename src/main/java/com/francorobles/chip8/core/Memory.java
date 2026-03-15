33ad75c32e7648b1a0176373a849272d434a1fd7
Franco Robles <franco_robles94@outlook.com>
2026-03-15 11:29:58 -0300

    implementacion de la memoria RAM

--- src/main/java/com/francorobles/chip8/core/Memory.java
+++ src/main/java/com/francorobles/chip8/core/Memory.java
@@ -1,4 +1,33 @@
 package com.francorobles.chip8.core;
 
+import java.util.ArrayList;
+
 public class Memory {
+    //4096 posicioes de 8bits cada una
+    private byte[] ram;
+
+    public Memory (){
+        this.ram = new byte[4096];
+
+    }
+
+    public void write(int address, byte value){
+        if(address>=0 && address<4096){
+            this.ram[address] = value;
+        }else{
+            System.err.println("Intento de escritura fuera de la memoria en: " + address);
+        }
+    }
+
+    public int read(int address){
+        // Se aplica una máscara a nivel de bits (bitwise AND)
+        // para convertir el byte con signo a un valor sin signo
+        if(address>=0 && address<4096){
+            return this.ram[address] & 0xFF;
+        }else{
+            System.err.println("Intento de escritura fuera de la memoria en: " + address);
+            return 0;
+        }
+
+    }
 }
